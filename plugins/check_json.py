5268216bed98ccea6964335b37b250e33a026ccd
Phillipe Smith <phillipelnx@gmail.com>
2018-09-13 14:31:33 -0300

    Changed optparse to argparse and urllib2 to requests

--- plugins/check_json.py
+++ plugins/check_json.py
@@ -18,33 +18,30 @@
 #     $./check_json.py -u http://date.jsontest.com/
 #      JSON Status API OK - date: 07-12-2014, milliseconds_since_epoch: 1405126483908, time: 12:54:43 AM
 #
-# TODO:
-#     Add Warning and Critical configuration
-#
 
 import json
 import sys
 import re
-from optparse import OptionParser
-from urllib2 import urlopen, Request, URLError, HTTPError
+import requests
+from argparse import ArgumentParser
 
-parser = OptionParser(usage='usage: %prog [ -u|--url http://json_result_url ] [ -f|--filter filter_expression ] [ -p|--perfdata ]')
-parser.add_option('-u', '--url', dest='url', help='JSON api url')
-parser.add_option('-f', '--filter', dest='filter', default='', help='Filter determined values. Ex.: "^tcp|^udp"')
-parser.add_option('-p', '--perfdata', dest='perfdata', default=False, help='Enable performance data. Must specify a expression with the values that going to be used as perfdata. If you want to show all values as perfdata put a "."')
-parser.add_option('-s', '--sort', dest='sort', action='store_true', help='Order output message alphabetically')
+#parser = ArgumentParser(usage='usage: %prog [ -u|--url http://json_result_url ] [ -f|--filter filter_expression ] [ -p|--perfdata ]')
+parser = ArgumentParser()
+parser.add_argument('-u', '--url', dest='url', help='JSON api url')
+parser.add_argument('-f', '--filter', dest='filter', default='', help='Filter determined values. Ex.: "^tcp|^udp"')
+parser.add_argument('-e', '--exclude', dest='exclude', help='Exclude a part of the metrics name. Ex.: "(counter|gauge)."')
+parser.add_argument('-s', '--sort', dest='sort', action='store_true', help='Order output message alphabetically')
+parser.add_argument('-t', '--threshold', dest='threshold', default='', help='Add thresholds for waning and critical. Ex.: counter:100:200')
+parser.add_argument('-p', '--perfdata', dest='perfdata', default='.', help='Filter perfdata to show only values matched by a regex')
+parser.add_argument('--no-perfdata', dest='no_perfdata', action='store_true', help='Disable perfdata')
 
-(option, args) = parser.parse_args()
+args = parser.parse_args()
 
 # Nagios status and messages
 nagios_status = ['OK', 'WARNING', 'CRITICAL', 'UNKNOW']
 
-filter   = option.filter
-perfdata = option.perfdata
-sort     = option.sort
-
 def exit(status, message):
-    print 'JSON Status API %s - %s' % (nagios_status[int(status)], message.encode('iso-8859-1'))
+    print '%s - %s' % (nagios_status[int(status)], message.encode('iso-8859-1'))
     sys.exit(status)
 
 def walk(data, levels = [], result = []):
@@ -53,54 +50,71 @@ def walk(data, levels = [], result = []):
             walk(item)
     elif isinstance(data, dict):
         for key, value in data.iteritems():
-            if isinstance(value, dict):
+            if isinstance(value, dict) or isinstance(value, list):
                 levels.append(key)
                 walk(value)
                 levels.pop()
             else:
                 prekeys = '.'.join(levels) + '.' if levels else ''
                 info    = '%s%s: %s' % (prekeys, key, value)
-                if re.search(filter, info):
+                if args.threshold:
+                    for threshold in args.threshold.split(','):
+                        threshold = threshold.split(':')
+                        if threshold[0] in key:
+                            if threshold[2] and value > float(threshold[2]):
+                                info  += '(!!);%s;%s;' % (threshold[1], threshold[2])
+                            elif threshold[1] and value > float(threshold[1]):
+                                info  += '(!);%s;%s;' % (threshold[1], threshold[2])
+
+                if re.search(args.filter, info):
                     result.append(info)
     return result
 
 def output(data):
+    status   = 0
     perf     = []
     textinfo = walk(data)
-    
-    if perfdata:
-        for value in textinfo:
-            if re.match(perfdata, value, re.IGNORECASE):
-                perf.append(value.replace(': ', '=') + ';;')
+
+    for value in textinfo:
+        if re.match(args.perfdata, value, re.IGNORECASE):
+            value = re.sub('\(!!?\)', '', value.replace(': ', '='))
+            perf.append(value+';')
+
+    if args.exclude:
+        textinfo = map(lambda x: re.sub('('+args.exclude+'|;.*)','',x), textinfo)
+        perf     = map(lambda x: re.sub(args.exclude,'',x), perf)
 
     if not textinfo:
         exit(3, 'No value information with the filter specified.')
 
-    if sort:
+    if args.sort:
         message = ', '.join(sorted(textinfo))
         perf    = ' '.join(sorted(perf))
     else:
         message = ', '.join(textinfo)
         perf    = ' '.join(perf)
 
-    if perf:
-        return exit(0, message + ' | ' + perf)
-    else:
-        return exit(0, message)
+    if '(!!)' in message:
+        status = 2
+    elif '(!)' in message:
+        status = 1
 
-if not option.url:
+    if args.no_perfdata == False:
+        return exit(status, message + ' | ' + perf)
+    else:
+        return exit(status, message)
+
+if not args.url:
     exit(3, 'Missing command line arguments')
 
 try:
-    request  = Request(option.url)
-    response = urlopen(request)
-except URLError as err:
-    exit(3, 'Url request error. %s' % err)
-except HTTPError as err:
-    exit(3, 'Invalid Uri. %s' % err.reason)
+    requests.packages.urllib3.disable_warnings()
+    response = requests.get(args.url, verify=False)
+except Exception, err:
+    exit(3, 'Error: %s' % err)
 else:
     try:
-        json_response = json.loads(response.read().decode('iso-8859-1'))
+        json_response = response.json()
     except Exception, e:
         exit(3, 'Invalid JSON response. %s' % e)
 
